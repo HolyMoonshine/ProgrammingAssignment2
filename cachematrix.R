f23233251e8e2c411cf6e98b89ef4ec4c2cb059d
HolyMoonshine <andrew@forgecycle.com>
2015-05-24 13:35:18 -0600

    Completed Coursera R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix provides a list of functions capable of 
+## storing a matrix, passing that matrix value to the 
+## cacheSolve function for futher computations and 
+## returns the computed value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL  #initialize m object
+  
+  ## set's matrix variable m to NULL when new matrix called
+  ## and assigns new matrix to x.
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## retuns matrix value stored in x
+  get <- function() x
+  
+  ## stores the inverse matrix value from cacheSolve
+  setinverse <- function(invm) m <<- invm
+  
+  ## returns either cached or newly calculated inverse matrix
+  getinverse <- function() m
+  
+  ## list of functions in function makeCacheMatrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## uses functions in makeCacheMatrix to to examine whether there
+## is an existing inverse matrix calculation value, calculates
+## a new one if there is not and returns the new inverse calculation
+## or simply passes back the existing inverse matrix value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## calls the getinverse function from makeCacheMatrix and
+  ## assigns value to m
+  m <- x$getinverse()
+  
+  ## if getinverse does not return a NULL value
+  ## then function returns m value from getinverse
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## otherwise, data object is assigned new matrix value
+  data <- x$get()
+  
+  ## m is assigned the inverse matrix calcuation value
+  m <- solve(data, ...)
+  
+  ## setinverse function is called with new inverse matrix result and 
+  ## returns m
+  x$setinverse(m)
+  m
 }
+
